cef6a526ef65c07bc9bebe67630562e4be912109
David Rubin <idf775@gmail.com>
2026-04-20 02:40:31 +0300

    add db connection

---
+++ db.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+_client = None
+_db = None
+
+def init_db(app):
+    global _client, _db
+    mongo_uri = os.getenv("MONGO_URI")
+    _client = MongoClient(mongo_uri)
+    _db = _client["job_tracker_db"]
+    app.config["DB"] = _db
+
+def get_collection(name):
+    return _db[name]
